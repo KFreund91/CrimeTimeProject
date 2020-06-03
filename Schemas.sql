26792eeeacde629fd4965cb2e2a25122566d0045
johnsonma23 <59981851+johnsonma23@users.noreply.github.com>
2020-06-03 19:35:12 -0400

    schema for tables

---
+++ Schemas.sql
@@ -0,0 +1,41 @@
+Drop Table Personal_Crime_09_13;
+Create Table Personal_Crime_09_13(
+	year int,
+	sex varchar(225),
+	race varchar(225),
+	hispanic_origin varchar(225),
+	age varchar(225),
+	marital_status varchar(225),
+	household_income varchar(225),
+	population_size varchar(225),
+	region varchar(225),
+	residence_location varchar(225),
+	victim_offender_relationship varchar(225),
+	weapon_category varchar (225),
+	crime_type varchar (225),
+	injury varchar(25),
+	incident_location varchar(225)
+	);
+
+
+
+
+
+Drop Table Personal_Crime_14_18;
+Create Table Personal_Crime_14_18(
+	year int,
+	sex varchar(225),
+	race varchar(225),
+	hispanic_origin varchar(225),
+	age varchar(225),
+	marital_status varchar(225),
+	household_income varchar(225),
+	population_size varchar(225),
+	region varchar(225),
+	residence_location varchar(225),
+	victim_offender_relationship varchar(225),
+	weapon_category varchar (225),
+	crime_type varchar (225),
+	injury varchar(25),
+	incident_location varchar(225)
+	);
